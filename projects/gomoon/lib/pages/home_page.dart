02f8a6b1903851f47cb653a728fae138dc36300a
Zain Ul Abdeen <zu4425@gmail.com>
2025-03-24 21:42:50 +0500

    project/fomoon updated

--- projects/gomoon/lib/pages/home_page.dart
+++ projects/gomoon/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  late double _deviceHeight , _devicewidth;
+  late double _deviceHeight, _devicewidth;
 
   Home({super.key});
 
@@ -11,27 +11,50 @@ class Home extends StatelessWidget {
     _devicewidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body:  SafeArea(child: Container(
-        height: _deviceHeight,
-        width: _devicewidth,
-        child: _pageTitle(),
-      ))
+        body: SafeArea(
+            child: Container(
+      height: _deviceHeight,
+      width: _devicewidth,
+      padding: EdgeInsets.symmetric(horizontal: _devicewidth * 0.05),
+      child: _pageTitle(),
+    )));
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "Gomoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
     );
   }
 
-   Widget _pageTitle (){
-    return const Text("Gomoon",style: TextStyle(color:Colors.white,
-        fontSize: 70,
-      fontWeight: FontWeight.w800
-    ), );
-   }
-  Widget _austroimagewidget ()
-  {
-   return   Container(
-      decoration:  const BoxDecoration(
-        image:  DecorationImage(
+  Widget _austroimagewidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
           fit: BoxFit.fill,
-          image: AssetImage("assets/images/astro_moon.png"),),
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _dropdownwediget() {
+    List <DropdownMenuItem<String>> _items = [
+      'My  name is',
+      'Zain Ul Abideen'
+    ].map((e) {
+      return DropdownMenuItem(child: Text(e), value: e,);
+    }).toList();
+  }
+
+
+    return Container(
+
+  child: DropdownButton(
+     onChanged: (_){},
+     items: items,
+
       ),
     );
   }
