b61ce1fde3d07afae3e831827d7b4f5d2f667f8f
Zain Ul Abdeen <zu4425@gmail.com>
2025-04-05 22:46:34 +0500

    project /TaskApp updated

--- projects/TaskApp/lib/pages/home_page.dart
+++ projects/TaskApp/lib/pages/home_page.dart
@@ -8,8 +8,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  late  double  _deviceHeight,_deviceWidth;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    
+    return  Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight  * 0.15,
+        title: Text('Task App',style: TextStyle(fontSize: 25),),
+      ),
+    );
   }
 }
